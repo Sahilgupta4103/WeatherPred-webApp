fa5850752ee965737bf837d236389a8872f125a3
Sahil Gupta <87527245+Sahilgupta4103@users.noreply.github.com>
2023-05-19 15:32:07 +0530

    Final Changes done

--- app.py
+++ app.py
@@ -5,23 +5,23 @@ import pickle
 # create the flask app
 app = Flask(__name__)
 
-
 # load the pickle model
 model = pickle.load(open("model.pkl", "rb"))
 
 
 # defining the homepage
-@app.route("/")  # app will take you to homepage
-def ome():
-    return render_template("index.html")
+@app.route("/")
+def home():
+    return render_template("APIindex.html")
 
 
 # defining the predict page
 @app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["POST"])
 def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict(features)
+    input = request.get_json()
+    features = [np.array(list(input.values()))]
+    prediction = model.predict(features)[0]
     print(features)
     if prediction == 0:
         prediction = "Drizzle"
@@ -34,10 +34,7 @@ def predict():
     elif prediction == 4:
         prediction = "Sunny"
 
-    return render_template(
-        "index.html",
-        prediction_text="The weather of your city is {}".format(prediction),
-    )
+    return jsonify({"prediction_text": "{}".format(prediction)})
 
 
 if __name__ == "__main__":
